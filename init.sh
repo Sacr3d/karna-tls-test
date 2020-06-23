d03c96d67e132a44ccca8e031437c4fc318ad4d1
Mattia Sassone <11msassone99@gmail.com>
2020-06-23 15:36:38 +0100

    removed old init.sh and changed peer1.sh to init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,15 @@ main() {
 
     sleep 5
 
+    launchTlsCaClient
+
+    sleep 5
+
     launchOrg0CaServer
+
+    sleep 5
+
+    launchOrg0CaClient
 
     sleep 5
 
@@ -14,88 +22,117 @@ main() {
 
     sleep 5
 
+    launchOrg1CaClient
+
+    sleep 5
+
     launchOrg2CaServer
 
-}
+    sleep 5
 
-registerTlsServerAdmin() {
-
-    docker exec -it karna-tls-ca-server /./scripts/enroll.sh
+    launchOrg2CaClient
 
 }
 
-launchTlsCaServer() {
+function launchTlsCaClient() {
 
-    pushd images/tls/
+    pushd docker-compose/tls/ca-client || return
+
+    docker-compose -p karna-tls-ca-client up -d
+
+    popd || exit
+
+}
+
+function launchTlsCaServer() {
+
+    pushd docker-compose/tls/ca-server || return
 
     docker-compose -p karna-tls-ca-server up -d
 
     sleep 5
 
-    registerTlsServerAdmin
+    docker exec -it karna-tls-ca-server cp /etc/hyperledger/fabric-ca-server/ca-cert.pem /shared/tls-ca-cert.pem
 
-    popd
-
-}
-
-registerorg0ServerAdmin() {
-
-    docker exec -it karna-org0-ca-server /./scripts/enroll.sh
+    popd || exit
 
 }
 
-launchOrg0CaServer() {
+function launchOrg0CaClient() {
 
-    pushd images/org0/ca-server/
+    pushd docker-compose/org0/host2/ca-client || return
+
+    docker-compose -p karna-org0-ca-client up -d
+
+    popd || exit
+
+}
+
+function launchOrg0CaServer() {
+
+    pushd docker-compose/org0/host1/ca-server || return
 
     docker-compose -p karna-org0-ca-server up -d
 
     sleep 5
 
-    registerorg0ServerAdmin
+    docker exec -it karna-org0-ca-server cp /etc/hyperledger/fabric-ca-server/ca-cert.pem /shared/org0-ca-cert.pem
 
-    popd
-
-}
-
-registerorg1ServerAdmin() {
-
-    docker exec -it karna-org1-ca-server /./scripts/enroll.sh
+    popd || exit
 
 }
 
-launchOrg1CaServer() {
+function launchOrg1CaClient() {
 
-    pushd images/org1/ca-server/
+    pushd docker-compose/org1/host2/ca-client || return
+
+    docker-compose -p karna-org1-ca-client up -d
+
+    popd || exit
+
+}
+
+function launchOrg1CaServer() {
+
+    pushd docker-compose/org1/host1/ca-server || return
 
     docker-compose -p karna-org1-ca-server up -d
 
     sleep 5
 
-    registerorg1ServerAdmin
+    docker exec -it karna-org1-ca-server cp /etc/hyperledger/fabric-ca-server/ca-cert.pem /shared/org1-ca-cert.pem
 
-    popd
-
-}
-
-registerorg2ServerAdmin() {
-
-    docker exec -it karna-org2-ca-server /./scripts/enroll.sh
+    popd || exit
 
 }
 
-launchOrg2CaServer() {
+function launchOrg2CaClient() {
 
-    pushd images/org2/ca-server/
+    pushd docker-compose/org2/host2/ca-client || return
+
+    docker-compose -p karna-org2-ca-client up -d
+
+    popd || exit
+
+}
+
+function launchOrg2CaServer() {
+
+    pushd docker-compose/org2/host1/ca-server || return
 
     docker-compose -p karna-org2-ca-server up -d
 
     sleep 5
 
-    registerorg2ServerAdmin
+    docker exec -it karna-org2-ca-server cp /etc/hyperledger/fabric-ca-server/ca-cert.pem /shared/org2-ca-cert.pem
 
-    popd
+    popd || exit
 
 }
+
+docker network create -d bridge karna-dev-net
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 
 main
